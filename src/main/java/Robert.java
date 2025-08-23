dbcc1f53508c061eb780fd6fcf677c9eb357cf8e
winstonzhaozhekai <winstonzhaozhekai@gmail.com>
2025-08-23 20:09:09 +0800

    Level-1

--- src/main/java/Robert.java
+++ src/main/java/Robert.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Robert {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Robert");
+        System.out.println(" Wassup chat! I'm Robert");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = sc.nextLine();
+
+            System.out.println("____________________________________________________________");
+            if (input.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println(" " + input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        sc.close();
     }
 }
