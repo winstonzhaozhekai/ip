89c5a1e2e450eaab0c5d3a29a08f25474df3f17f
winstonzhaozhekai <winstonzhaozhekai@gmail.com>
2025-08-23 20:13:46 +0800

    Level-2

--- src/main/java/Robert.java
+++ src/main/java/Robert.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Robert {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int count = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println(" Wassup chat! I'm Robert");
@@ -17,8 +19,18 @@ public class Robert {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
             } else {
-                System.out.println(" " + input);
+                if (count >= tasks.length) {
+                    System.out.println(" Sorry, the list has hit limit of 100 items.");
+                } else {
+                    tasks[count++] = input;
+                    System.out.println(" added: " + input);
+                }
                 System.out.println("____________________________________________________________");
             }
         }
